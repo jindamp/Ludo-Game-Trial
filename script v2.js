ccbb013e048c0146931d019f134e04e848146cbd
pavan <jindmpavan@gmail.com>
2020-05-12 13:27:57 +0530

    working app

--- script v2.js
+++ script v2.js
@@ -3,24 +3,36 @@ var app2 = new Vue({
   mounted: function () {
     var parent = this;
     this.operator = this.nothing;
-  },
-  methods: {
-    drawBoxColor: function (i) {
-      var parent = this;
-      this.operator();
+
+    this.corners();
+    var i = 0;
+
+    loop();
+
+    function loop() {
+      parent.operator();
+
       try {
         var box = document.getElementById(parent.startx + "," + parent.startY);
-        $("div.active").removeClass("active");
-        box.classList.add("active");
-        // box.style.backgroundColor = "#7090ea";
-        // box.style.color = "white";
+        box.style.backgroundColor = "#7090ea";
+        box.style.color = "white";
         box.innerHTML += i;
         box.setAttribute("rel", i);
       } catch {}
-    },
-    move: function (i) {
-      this.i++;
-      this.drawBoxColor(i);
+
+      parent.printxyCorners();
+      parent.corners(i);
+
+      setTimeout(function () {
+        if (i < 52) {
+          i++;
+          loop();
+        }
+      }, 25);
+    }
+  },
+  methods: {
+    corners: function (i) {
       // is in home ...
       var homeArr = [66, 67, 68, 77, 78, 79, 86, 87, 88];
       var pos = this.startx + ":" + this.startY;
@@ -29,38 +41,34 @@ var app2 = new Vue({
       switch (pos) {
         case "1:8":
         case "6:13":
-        case "6:6":
-        case "6:12":
+        case "6:6": // y--
           this.operator = this.yminus;
           this.printxyCorners();
           break;
 
         case "1:6":
         case "6:1":
-        case "8:6":
-        case "2:6": // x++
+        case "8:6": // x++
           this.operator = this.xplus;
           this.printxyCorners();
           break;
 
         case "8:1":
         case "13:6":
-        case "8:8":
-        case "8:2": // y++
+        case "8:8": // y++
           this.operator = this.yplus;
           break;
 
         case "13:8":
         case "8:13":
-        case "6:8":
-        case "12:8": // x--
+        case "6:8": // x--
           this.operator = this.xminus;
           break;
 
         case "1:7":
           var startIndex = document.querySelectorAll('[rel="0"]')[0].id;
-          if (startIndex == "2,6") {
-            this.operator = this.xplus;
+          if (startIndex == "13,7") {
+            this.operator = this.xminus;
           }
           break;
         case "7:1":
@@ -73,12 +81,6 @@ var app2 = new Vue({
           var startIndex = document.querySelectorAll('[rel="0"]')[0].id;
           if (startIndex == "12,8") {
             this.operator = this.xminus;
-          }
-          break;
-        case "7:13":
-          var startIndex = document.querySelectorAll('[rel="0"]')[0].id;
-          if (startIndex == "6,12") {
-            this.operator = this.yminus;
           }
           break;
         case "13:10":
@@ -108,11 +110,10 @@ var app2 = new Vue({
   data: {
     x: 13,
     y: 13,
-    startx: 8,
-    startY: 2,
+    startx: 6,
+    startY: 12,
     count: 0,
     startPos: "2:6",
     operator: "test",
-    i: 0,
   },
 });
